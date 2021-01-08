bcb1859c2bb35f4029411c85eecc8209ec326e22
larryebaum <6776768+larryebaum@users.noreply.github.com>
2021-01-08 12:59:01 -0500

    init

---
+++ main.tf
@@ -0,0 +1,84 @@
+provider "aws" {
+  region = var.aws_region
+}
+
+locals {
+  common_tags = {
+    owner = var.owner
+    se-region = var.se-region
+    purpose = var.purpose
+    ttl = var.ttl #-1 must has justification as purpose
+    creator = var.name
+    customer = var.customer
+    Name = "${var.owner}-${var.purpose}-${var.customer}"
+  }
+}
+
+data "aws_availability_zones" "available" {
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "172.17.0.0/16"
+  tags = local.common_tags
+}
+
+resource "aws_subnet" "private" {
+  count             = var.az_count
+  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+  vpc_id            = aws_vpc.main.id
+  tags = local.common_tags
+}
+
+resource "aws_subnet" "public" {
+  count                   = var.az_count
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 8, var.az_count + count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  vpc_id                  = aws_vpc.main.id
+  map_public_ip_on_launch = true
+  tags = local.common_tags
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+  tags = local.common_tags
+}
+
+resource "aws_route" "internet_access" {
+  route_table_id         = aws_vpc.main.main_route_table_id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+  tags = local.common_tags
+}
+
+resource "aws_eip" "gw" {
+  count      = var.az_count
+  vpc        = true
+  depends_on = [aws_internet_gateway.gw]
+  tags = local.common_tags
+}
+
+resource "aws_nat_gateway" "gw" {
+  count         = var.az_count
+  subnet_id     = element(aws_subnet.public.*.id, count.index)
+  allocation_id = element(aws_eip.gw.*.id, count.index)
+  tags = local.common_tags
+}
+
+resource "aws_route_table" "private" {
+  count  = var.az_count
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = element(aws_nat_gateway.gw.*.id, count.index)
+  }
+  tags = local.common_tags
+}
+
+resource "aws_route_table_association" "private" {
+  count          = var.az_count
+  subnet_id      = element(aws_subnet.private.*.id, count.index)
+  route_table_id = element(aws_route_table.private.*.id, count.index)
+  tags = local.common_tags
+}
---
+++ variables.tf
@@ -0,0 +1,55 @@
+variable "aws_region" {
+  default = "eu-west-1"
+  description = "The AWS region to deploy to"
+}
+
+variable "az_count" {
+  default = 2
+  description = "Number of availability zones to use within the region"
+}
+variable "prefix" {
+  default = "tfe"
+}
+
+variable "availability_zone" {
+  default = "us-west-2a"
+}
+
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+variable "instance_type" {
+  description = "type of EC2 instance to provision."
+  default = "t2.micro"
+}
+
+variable "name" {
+  description = "name to pass to name tag"
+  default = "Larry Eichenbaum"
+}
+
+variable "owner" {
+  description = "name to pass to owner tag"
+  default = "larryebaum"
+}
+
+variable "ttl" {
+  description = "ttl to pass to ttl tag"
+  default = "4"
+}
+
+variable "se-region" {
+  description = "SE region assigned"
+  default = "public-sector"
+}
+
+variable "purpose" {
+  description = "Purpose; Required if TTL = -1"
+  default = "demo"
+}
+
+variable "customer" {
+  description = "Billable Customer"
+  default = ""
+}
